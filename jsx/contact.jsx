02e92a9de8426cc7d537276533e277d00dd52726
Pina Gomet <pina@pinas-mbp.myfiosgateway.com>
2022-08-11 15:54:54 -0400

    added in resume png

--- jsx/contact.jsx
+++ jsx/contact.jsx
@@ -18,7 +18,7 @@ const App = () => (
         <Card source="../assets/images/github.png" text={<a href="https://github.com/pgomet" target="_blank" rel="noopener noreferrer"><button type="button" className="btn btn-dark">GitHub</button></a>} />
         <Card source="../assets/images/linkedin.png" text={<a href="https://www.linkedin.com/in/pinagomet/" target="_blank" rel="noopener noreferrer"><button type="button" className="btn btn-dark">LinkedIn</button></a>} />
         <Card
-          source="ADD IN PNG OF RESUME"
+          source="../assets/images/resume.png"
         />
         <Card source="../assets/images/phone.png" text="860-899-7108" />
       </div>
